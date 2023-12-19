bab654e1a60768ea73e8ae27e088b6623581396f
Jayanti2919 <jayanti2919@gmail.com>
2023-12-19 21:45:20 +0530

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='text-5xl font-bold'>App</div>
   )
 }
+
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <ThirdwebProvider chainId={ChainId.Goerli}>
+        <Router>
+        <App />
+        </Router>
+    </ThirdwebProvider>
+);
